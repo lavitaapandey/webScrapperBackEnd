f9808f2cb28db9ae1ab5656084aba65049b55fd9
lavitaapandey <lavita.techbit@outlook.com>
2020-10-15 19:30:18 +0530

    inital commit

---
+++ WebScrapper/Models/ScrapperData.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace WebScrapper.Models
+{
+    public class ScrapperData
+    {
+        public int Id { get; set; }
+        public string siteName { get; set; }
+        public string metaDescription { get; set; }
+        public string metaKeywords { get; set; }
+        public string imagePath { get; set; }
+        public string title { get; set; }
+        public List<string> hyperlinks { get; set; }
+        
+    }
+}
